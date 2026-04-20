a911bf394810c15adb792676a848edd0cf07f52e
tknurture <tknurture@gmail.com>
2026-04-20 21:37:28 +0200

    Re-add rozbor vody as Rozbor vody z vlastní studny

--- app/page.tsx
+++ app/page.tsx
@@ -1016,6 +1016,12 @@ export default function Home() {
             </a>
           </li>
           <li>
+            <a href="/documents/rozbor-vody.pdf" download>
+              <svg viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="12" y1="18" x2="12" y2="12"/><polyline points="9 15 12 18 15 15"/></svg>
+              Rozbor vody z vlastní studny
+            </a>
+          </li>
+          <li>
             <a href="/documents/penb.pdf" download>
               <svg viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="12" y1="18" x2="12" y2="12"/><polyline points="9 15 12 18 15 15"/></svg>
               PENB
