67ccf15f659c5db17656cf51522639334e739d1a
tknurture <tknurture@gmail.com>
2026-04-20 21:43:05 +0200

    Rename to Dokumentace skutečného provedení stavby

--- app/page.tsx
+++ app/page.tsx
@@ -1036,7 +1036,7 @@ export default function Home() {
           <li>
             <a href="/documents/dokumentace-stavby.pdf" download>
               <svg viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="12" y1="18" x2="12" y2="12"/><polyline points="9 15 12 18 15 15"/></svg>
-              Dokumentace skutečné stavby
+              Dokumentace skutečného provedení stavby
             </a>
           </li>
         </ul>
